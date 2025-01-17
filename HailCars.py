acffb433a8d0f4d7c4f51b0911937a72911c2fd2
Emad U Din soomro <soomroemadnexus@gmail.com>
2025-01-17 12:40:37 +0500

    Add files via upload

---
+++ HailCars.py
@@ -0,0 +1,310 @@
+import time
+from utils import Handywrapper
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import json
+import psycopg2
+from psycopg2 import sql
+from datetime import datetime
+
+class HailCars:
+    # PW_Locations =["Karachi", "Lahore", "Islamabad", "Rawalpindi", "Peshawar", "Quetta","Abbottabad","Bahawalpur","Faisalabad","Gujranwala","Haripur", "Hyderabad","Jhelum", "Kashmir", "Larkana","Mian Wali", "Mirpur khas", "Multan","Murree", "Muzaffar Gargh", "Muzaffarabad", "Nawabshah","Rahim Yar Khan","Sadiqabad","Sahiwal","Sargodha","Sialkot","Sukkur","Taxila"]
+    PW_Locations =["Lahore", "Islamabad", "Hyderabad","Multan"]
+    # Makes = ["Toyota", "Suzuki", "Honda", "Daihatsu", "Adam", "Audi", "BAIC", "BMW", "BYD", "Bentley",
+    # "Buick", "Cadillac", "Changan", "Chery", "Chevrolet", "Chrysler", "DFSK", "Daehan", "Daewoo",
+    # "Datsun", "Deepal", "Dodge", "FAW", "Fiat", "Ford", "GMC", "GUGO", "Geely", "Genesis",
+    # "Golden Dragon", "Haval", "Hino", "Honri", "Hummer", "Hyundai", "Isuzu", "JAC", "JW Forland",
+    # "Jaguar", "Jeep", "KIA", "Lamborghini", "Land Rover", "Lexus", "MG", "MINI", "Master",
+    # "Mazda", "Mercedes Benz", "Mitsubishi", "Nissan", "ORA", "Opel", "Peugeot", "Porsche", "Power",
+    # "Prince", "Proton", "Range Rover", "Renault", "Rinco", "Rolls Royce", "Sogo", "SsangYong",
+    # "Subaru", "Tank", "Tesla", "United", "Volkswagen", "Volvo", "Willys", "ZOTYE"]
+
+    def pakwheels(self):
+        database_cred = "postgresql://saim:R2_RkmUt3xc59Gjzuhn33A@joking-egret-7111.8nk.cockroachlabs.cloud:26257/HailCars?sslmode=require"
+
+        conn = psycopg2.connect(database_cred)
+        cursor = conn.cursor()
+        table_name = "pakwheels"
+        self.create_table(conn, cursor, table_name)
+
+        Locations=self.PW_Locations
+        # Makes = self.Makes
+
+        wd = webdriver.Chrome()
+        hw = Handywrapper(wd)
+        for location in Locations:
+            wd.get("https://www.pakwheels.com/used-cars")
+            hw.Click_element(By.XPATH," //span[.='All Cities']//parent::a[@href='javascript:void(0)']")
+            serach_city = hw.find_element(By.XPATH,"//span[.='All Cities']/parent::a//following-sibling::div/div[@class='chzn-search']/input")
+
+            serach_city.send_keys(location)
+            hw.Click_element(By.XPATH, f"//li[.='{location}' and contains(@id,'Used')]")
+            hw.Click_element(By.XPATH,"//button[.='Search']")
+            time.sleep(1)
+            loading = hw.is_element_present(By.XPATH,"//i[@class='ajax-search-loader search-loader-fixed' and not @style='display: none;']")
+            while loading:
+                loading = hw.is_element_present(By.XPATH,"//i[@class='ajax-search-loader search-loader-fixed' and not @style='display: none;']")
+                time.sleep(1)
+            hw.Click_element(By.XPATH,"//a[contains(.,'Make')]/parent::div/following-sibling::div/div/span[contains(.,'more choices')]")
+            hw.wait_explicitly(By.XPATH, "//li//input[@name='checkbox']")
+            Makes = hw.get_list_of_attributes(By.XPATH, "//li//input[@name='checkbox']")
+            Makes = list(map(lambda x: x.strip(), Makes))
+            for make in Makes:
+                time.sleep(0.5)
+                hw.Click_element(By.XPATH,"//a[contains(.,'Make')]/parent::div/following-sibling::div/div/span[contains(.,'more choices')]")
+                hw.Click_element(By.XPATH,"//button[.='NO THANKS']")
+                hw.Click_element(By.XPATH,"//button[.='Clear']")
+                time.sleep(0.5)
+                hw.Click_element(By.XPATH,f"//input[@value='{make.lower()}' and @name='checkbox']")
+
+                hw.Click_element(By.XPATH,"//button[@value='submit' and .='Submit']")
+                time.sleep(1)
+                loading = hw.is_element_present(By.XPATH,"//i[@class='ajax-search-loader search-loader-fixed' and not @style='display: none;']")
+                while loading:
+                    loading = hw.is_element_present(By.XPATH,"//i[@class='ajax-search-loader search-loader-fixed' and not @style='display: none;']")
+                    print(loading)
+                    time.sleep(1)
+
+                hw.Click_element(By.XPATH,"//a[contains(.,'Model')]/parent::div/following-sibling::div/div/span[contains(.,'more choices')]")
+                hw.wait_explicitly(By.XPATH,"//li//input[@name='checkbox']")
+                Models = hw.get_list_of_attributes(By.XPATH,"//li//input[@name='checkbox']")
+                Models = list(map(lambda x:x.strip(), Models))
+                print(Models)
+                for model in Models:
+                    hw.Click_element(By.XPATH,"//a[contains(.,'Model')]/parent::div/following-sibling::div/div/span[contains(.,'more choices')]")
+                    hw.Click_element(By.XPATH, "//button[.='Clear']")
+                    hw.Click_element(By.XPATH, f"//input[@value='{model.lower()}' and @name='checkbox']")
+                    hw.Click_element(By.XPATH, "//button[@value='submit' and .='Submit']")
+                    loading = hw.is_element_present(By.XPATH, "//i[@class='ajax-search-loader search-loader-fixed' and not @style='display: none;']")
+                    while loading:
+                        loading = hw.is_element_present(By.XPATH,"//i[@class='ajax-search-loader search-loader-fixed' and not @style='display: none;']")
+                        time.sleep(1)
+
+                    time.sleep(1.5)
+
+                    html_content = wd.page_source
+                    soup = BeautifulSoup(html_content, 'html.parser')
+
+                    script_tags = soup.find_all('script', type='application/ld+json')
+                    script_tags =script_tags[2:]
+                    parsed_data = []
+                    for idx, script_tag in enumerate(script_tags, start=1):
+                        item = {}
+                        try:
+                            item = json.loads(script_tag.string)
+
+                        except json.JSONDecodeError:
+                            print(f"Error decoding JSON in script tag {idx}")
+                        vehicle_details = {
+                            "Brand": hw.get_value_by_key("name",hw.get_value_by_key("brand",item)),
+                            "Description": hw.get_value_by_key("description",item),
+                            "Condition": hw.get_value_by_key("itemCondition",item),
+                            "Model Year": hw.get_value_by_key("modelDate",item),
+                            "Manufacturer": hw.get_value_by_key("manufacturer",item),
+                            "Fuel Type": hw.get_value_by_key("fuelType",item),
+                            "Transmission": hw.get_value_by_key("vehicleTransmission",item),
+                            "Engine": hw.get_value_by_key("engineDisplacement",hw.get_value_by_key("vehicleEngine",item)),
+                            "Mileage": hw.get_value_by_key("mileageFromOdometer",item),
+                            "Price": hw.get_value_by_key("price",hw.get_value_by_key("offers", item)),
+                            "Currency": hw.get_value_by_key("priceCurrency",hw.get_value_by_key("offers", item)),
+                            "URL": hw.get_value_by_key("url",hw.get_value_by_key("offers",item)),
+                            "Image": hw.get_value_by_key("image",item),
+                            "City": location,
+                            "Model": model.Capitalize()
+                        }
+                        if vehicle_details["Brand"] != "" and vehicle_details["Description"] != "" and vehicle_details["Manufacturer"] !="":
+                            parsed_data.append(vehicle_details)
+
+                            data_to_insert = (
+                                str(vehicle_details['Brand']), str(vehicle_details['Description']), str(vehicle_details['Condition']),
+                                str(vehicle_details['Model Year']), str(vehicle_details['Manufacturer']), str(vehicle_details['Fuel Type']),
+                                str(vehicle_details['Transmission']), str(vehicle_details['Engine']), vehicle_details['Mileage'], vehicle_details['Price'],
+                                str(vehicle_details['Currency']), str(vehicle_details['URL']), vehicle_details['Image'], vehicle_details['City'], vehicle_details['Model'],
+                            datetime.now().isoformat())
+                            try:
+                                self.insert_data(conn, cursor,table_name, data_to_insert)
+                            except Exception as e:
+                                print(f"error: {e}")
+
+                hw.Click_element(By.XPATH,"//a[contains(.,'Model')]/parent::div/following-sibling::div/div/span[contains(.,'more choices')]")
+                hw.Click_element(By.XPATH, "//button[.='Clear']")
+                hw.Click_element(By.XPATH, "//button[@value='submit' and .='Submit']")
+
+                hw.Click_element(By.XPATH,"//a[contains(.,'Make')]/parent::div/following-sibling::div/div/span[contains(.,'more choices')]")
+                hw.Click_element(By.XPATH, "//button[.='Clear']")
+                hw.Click_element(By.XPATH, "//button[@value='submit' and .='Submit']")
+
+
+    def olx(self):
+        database_cred = "postgresql://saim:R2_RkmUt3xc59Gjzuhn33A@joking-egret-7111.8nk.cockroachlabs.cloud:26257/HailCars?sslmode=require"
+
+        conn = psycopg2.connect(database_cred)
+        cursor = conn.cursor()
+        table_name = "olx"
+        self.create_table(conn, cursor, table_name)
+        Locations = self.PW_Locations
+        makes = ["Suzuki", "Toyota", "Honda", "Daihatsu", "Nissan", "Mitsubishi", "KIA", "Changan", "Hyundai", "Mazda",
+                  "FAW", "MG", "Prince", "Mercedes", "Chevrolet", "Isuzu", "Subaru", "Proton", "Other Brands", "DFSK"]
+
+        wd = webdriver.Chrome()
+        hw = Handywrapper(wd)
+        for location in Locations:
+            wd.get("https://www.olx.com.pk/cars_c84")
+            hw.wait_explicitly(By.XPATH, f"(//span[contains(.,'{location}')])[1]")
+            hw.Click_element(By.XPATH, f"(//span[contains(.,'{location}')])[1]")
+            run =1
+            for make in makes:
+                time.sleep(0.5)
+                if run ==1:
+                    hw.Click_element(By.XPATH,f"//span[.='{make}']")
+                    time.sleep(1)
+                parsed_data = []
+                model_el = hw.find_elements(By.XPATH,"//div[.='Brand and Model']/following-sibling::div//label/preceding-sibling::input[@type='checkbox']")
+                for model_ind in range(len(model_el)):
+                    run +=1
+                    model_ind = model_ind + 1
+                    hw.Click_element(By.XPATH, f"//span[.='{make}']")
+                    time.sleep(1)
+                    model = hw.find_element(By.XPATH,f"(//div[.='Brand and Model']/following-sibling::div//label/preceding-sibling::input[@type='checkbox'])[{model_ind}]")
+                    hw.Click_element(element=model)
+                    time.sleep(0.5)
+                    model_text = hw.find_element_text(By_type=By.XPATH, locator=f"(//div[.='Brand and Model']/following-sibling::div//label/span)[{model_ind}]")
+                    time.sleep(0.5)
+                    items = hw.find_elements(By.XPATH, "//li[@aria-label='Listing']/article")
+                    for item_ind in range(len(items)):
+                        time.sleep(0.1)
+                        item_ind = item_ind + 1
+                        try:
+                            wd.execute_script(
+                                "arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", hw.find_element(By.XPATH,f"(//li[@aria-label='Listing']/article)[{item_ind}]/div//picture/source"))
+                        except:
+                            pass
+                        Description = hw.get_attribute(By.XPATH, f"(//li[@aria-label='Listing']/article)[{item_ind}]/div/div/a","title")
+                        Url = hw.get_attribute(By.XPATH, f"(//li[@aria-label='Listing']/article)[{item_ind}]/div/div/a","href")
+                        Image = hw.get_attribute(By.XPATH, f"(//li[@aria-label='Listing']/article)[{item_ind}]/div//picture/source", "srcset")
+                        Price = hw.find_element_text(By.XPATH,f"(//li[@aria-label='Listing']/article)[{item_ind}]/div/div/div[@aria-label='Price']/span")
+                        Mileage = hw.find_element_text(By.XPATH,f"(//li[@aria-label='Listing']/article)[{item_ind}]/div/div//span[@aria-label='Mileage']/span")
+                        ModelYear = hw.find_element_text(By.XPATH,f"(//li[@aria-label='Listing']/article)[{item_ind}]/div/div//span[@aria-label='Year']/span")
+                        FuelType = hw.find_element_text(By.XPATH,f"(//li[@aria-label='Listing']/article)[{item_ind}]/div/div//span[@aria-label='FuelType']/span")
+                        Currency = Price.split(' ')[0]
+                        Price = Price.split(' ')[-1]
+                        model = "".join(model_text.split("(")[:-1]).strip()
+
+                        vehicle_details = {
+                            "Brand": make,
+                            "Description": Description,
+                            "Condition": "used",
+                            "Model Year": ModelYear,
+                            "Manufacturer": make,
+                            "Fuel Type": FuelType,
+                            "Transmission": "",
+                            "Engine": "",
+                            "Mileage": Mileage,
+                            "Price": Price,
+                            "Currency": Currency,
+                            "URL": Url,
+                            "Image": Image,
+                            "City": location,
+                            "Model": model
+                        }
+                        if vehicle_details["Brand"] != "" and vehicle_details["Description"] != "" and vehicle_details[
+                            "Manufacturer"] != "":
+                            parsed_data.append(vehicle_details)
+
+                            data_to_insert = (
+                                str(vehicle_details['Brand']), str(vehicle_details['Description']),
+                                str(vehicle_details['Condition']),
+                                str(vehicle_details['Model Year']), str(vehicle_details['Manufacturer']),
+                                str(vehicle_details['Fuel Type']),
+                                str(vehicle_details['Transmission']), str(vehicle_details['Engine']),
+                                vehicle_details['Mileage'], vehicle_details['Price'],
+                                str(vehicle_details['Currency']), str(vehicle_details['URL']), vehicle_details['Image'], vehicle_details['City'], vehicle_details['Model'],
+                            datetime.now().isoformat())
+                            try:
+                                self.insert_data(conn, cursor, table_name, data_to_insert)
+                            except Exception as e:
+                                print(f"error: {e}")
+
+                    time.sleep(0.5)
+                    try:
+                        wd.execute_script(
+                            "arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});",
+                        hw.find_element(By.XPATH, "//a[.='Clear']"))
+                    except:
+                        pass
+                    hw.Click_element(By.XPATH, "//a[.='Clear']")
+                    time.sleep(1)
+
+
+    def create_table(self,conn, cursor, table_name):
+        table_name = sql.Identifier(table_name)
+        create_table_query = sql.SQL('''CREATE TABLE IF NOT EXISTS {} (
+            id SERIAL PRIMARY KEY,
+            brand VARCHAR(255),
+            description TEXT,
+            condition VARCHAR(50),
+            model_year VARCHAR(50),
+            manufacturer VARCHAR(255),
+            fuel_type VARCHAR(50),
+            transmission VARCHAR(50),
+            engine VARCHAR(50),
+            mileage VARCHAR(50),
+            price VARCHAR(50),
+            currency VARCHAR(10),
+            url TEXT UNIQUE,
+            image TEXT,
+            city VARCHAR(10),
+            model VARCHAR(255),
+            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );''').format(table_name)
+
+        cursor.execute(create_table_query)
+        conn.commit()
+
+    def insert_data(self, conn, cursor, table_name, data):
+        table_name = sql.Identifier(table_name)
+        insert_query = sql.SQL("""
+                INSERT INTO {} (
+                    brand,
+                    description,
+                    condition,
+                    model_year,
+                    manufacturer,
+                    fuel_type,
+                    transmission,
+                    engine,
+                    mileage,
+                    price,
+                    currency,
+                    url,
+                    image,
+                    city,
+                    model,
+                    timestamp
+                )
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                ON CONFLICT (url) DO UPDATE
+                SET 
+                    brand = EXCLUDED.brand,
+                    description = EXCLUDED.description,
+                    condition = EXCLUDED.condition,
+                    model_year = EXCLUDED.model_year,
+                    manufacturer = EXCLUDED.manufacturer,
+                    fuel_type = EXCLUDED.fuel_type,
+                    transmission = EXCLUDED.transmission,
+                    engine = EXCLUDED.engine,
+                    mileage = EXCLUDED.mileage,
+                    price = EXCLUDED.price,
+                    currency = EXCLUDED.currency,
+                    image = EXCLUDED.image,
+                    city = EXCLUDED.city,
+                    model = EXCLUDED.model,
+                    timestamp = EXCLUDED.timestamp;
+            """).format(table_name)
+
+        cursor.execute(insert_query, data)
+        conn.commit()
+
+HailCars = HailCars()
+HailCars.olx()
---
+++ utils.py
@@ -0,0 +1,128 @@
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+class Handywrapper:
+
+    def __init__(self,driver):
+        self.driver = driver
+
+
+    def find_element(self,By_type, locator=""):
+        element = ""
+        try:
+            element = self.driver.find_element(By_type, locator)
+            print(element)
+            return element
+        except:
+            return element
+
+
+    def get_attribute(self,By_type, locator="", att="value"):
+        value = ""
+        try:
+            element = self.find_element(By_type, locator)
+            value = element.get_attribute(att)
+            return value
+        except:
+            return value
+
+
+    def find_elements(self,By_type, locator=""):
+        element_list = []
+        try:
+            element_list = self.driver.find_elements(By_type, locator)
+            return element_list
+        except:
+            return element_list
+
+
+    def get_list_of_attributes(self,By_type, locator="", att="value"):
+        value_list = []
+        try:
+            element_list = self.find_elements(By_type, locator)
+            for element in element_list:
+                value = element.get_attribute(att)
+                value_list.append(value)
+            print(value_list)
+            return value_list
+        except:
+            return value_list
+
+
+    def get_element_tag(self, element=None, tag=""):
+        try:
+            element = element.find_element(By.TAG_NAME, tag)
+            return element
+        except:
+            return ""
+
+
+    def find_element_text(self,By_type="", locator="",element = None):
+        try:
+            if element !=None:
+                element_text = element.text
+            else:
+                time.sleep(0.5)
+                element = self.find_element(By_type, locator)
+                element_text = element.text
+            print(element_text)
+            return element_text
+        except:
+            return ""
+
+
+    def find_elements_list_of_text(self,By_type, locator=""):
+        elements_list_of_text = []
+        try:
+            elements = self.find_elements(By_type, locator)
+            for element in elements:
+                elements_list_of_text.append(element.text)
+            print(elements_list_of_text)
+            return elements_list_of_text
+        except:
+            return []
+
+
+    def Click_element(self,By_type="", locator="", element=None ):
+        try:
+            time.sleep(0.5)
+            if element is not None:
+                self.driver.execute_script("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", element)
+                time.sleep(0.5)
+                element.click()
+            else:
+                self.wait_explicitly(By_type, locator)
+                element = self.find_element(By_type, locator)
+                self.wait_explicitly(By_type, locator)
+                self.driver.execute_script("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", element)
+                time.sleep(0.5)
+                element.click()
+        except:
+            print("cannot click the element")
+
+
+    def is_element_present(self,By_type, locator=""):
+        try:
+            element = self.driver.find_element(By_type, locator)
+            if element is not None:
+                return True
+            else:
+                return False
+        except:
+            return False
+
+    def wait_explicitly(self, By_type, locator=""):
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By_type, locator)))
+        except:
+            pass
+
+    def get_value_by_key(self, key, dict):
+        try:
+            value = dict[key]
+            return value
+        except:
+            return ""
